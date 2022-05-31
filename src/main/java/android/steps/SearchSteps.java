12f52b6d86a5a2ee710bf91d18ff62b78dedc8cd
Uladzislau Tylkovich <vlad5grup@gmail.com>
2022-05-31 13:35:35 +0300

    uodate
    update

--- src/main/java/android/steps/SearchSteps.java
+++ src/main/java/android/steps/SearchSteps.java
@@ -2,7 +2,6 @@ package android.steps;
 
 import com.epam.jdi.light.mobile.elements.common.MobileDevice;
 import com.epam.jdi.light.mobile.elements.composite.MobileScreen;
-import com.jdiai.tools.Timer;
 import io.qameta.allure.Step;
 import org.openqa.selenium.ScreenOrientation;
 
@@ -34,8 +33,9 @@ public class SearchSteps {
 
     @Step("Scrool down")
     public void scroolDown() {
-      MobileScreen.scrollDown(2000);
+        MobileScreen.scrollDown(2000);
     }
+
     @Step("Scrol Botom")
     public void scroollBotom() {
         MobileScreen.scrollToBottom();
