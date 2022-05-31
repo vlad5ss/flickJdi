abfe1f95b94abd76c4b1986d52189784a354ae32
Uladzislau Tylkovich <vlad5grup@gmail.com>
2022-05-31 18:22:03 +0300

    update
    update

--- src/test/java/main/tests/FavesTest.java
+++ src/test/java/main/tests/FavesTest.java
@@ -11,13 +11,11 @@ public class FavesTest extends FlickrTestInit implements FlickS {
 
 
     @Test
-    public void tapSearchPhotoFawesTest() throws InterruptedException {
+    public void tapSearchPhotoFawesTest(){
         loginSteps.loginFlickr();
         navigationMenuSteps.tapSearch();
         searchSteps.tapPhoto();
         searchSteps.favesClick();
         fawesSteps.folowClick();
-        Thread.sleep(40000);
-       // assertThat(actulResult).as("Comment is the same").isEqualTo(commentPage.messageTxt.getText());
     }
 }
